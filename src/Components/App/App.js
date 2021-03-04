7d89d1c38d2b30eeb0577e8c110133d7677c5efa
Christina Cook <cmc.cook7@gmail.com>
2021-03-04 13:42:39 -0700

    Create header component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from '../Header/Header';
 import Game from '../Game/Game';
+import Home from '../Home/Home';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Header />
+      <Home />
       <Game />
     </div>
   );
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <h2>Header goes at the top</h2>
+  )
+}
+
+export default Header;
