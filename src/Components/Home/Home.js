df62d3185fd59a769cf4f1e5ec83bebdd3565be2
Christina Cook <cmc.cook7@gmail.com>
2021-03-04 19:59:30 -0700

    Link start button to game page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Home.css';
 import frame from '../../assets/frame.png';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div className='home-page'>
       <h2 className='home-header'>The One With All the Questions</h2>
       <img src={frame} alt='gold frame' className='frame'/>
-      <button className='start-button'>Get Started</button>
+      <Link to='/game' className='button-link'>
+        <button className='start-button'>Get Started</button>
+      </Link>
     </div>
   )
 }
