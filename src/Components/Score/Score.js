ee1e3e7b346eb8c16d6e7997440cfaff165cb454
Christina Cook <cmc.cook7@gmail.com>
2021-03-05 14:40:27 -0700

    Add links on score page

--- src/Components/Score/Score.js
+++ src/Components/Score/Score.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './Score.css';
+import { Link } from 'react-router-dom';
+
 
 const Score = () => {
   return (
+    <>
+    <div className='nav-buttons'>
+      <Link to='/'>
+        <button className='home'>Home</button>
+      </Link>
+      <Link to='/game'>
+      <button className='restart'>Start Over</button>
+      </Link>
+    </div>
     <h2>Score gets displayed here</h2>
+    </>
   )
 }
 
