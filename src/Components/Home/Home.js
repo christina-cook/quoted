e81f654e958c5f3e8c6a6dde07b26133918c14c4
Christina Cook <cmc.cook7@gmail.com>
2021-03-04 18:40:58 -0700

    Style home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Home.css';
+import frame from '../../assets/frame.png';
 
 const Home = () => {
   return (
-    <h2>I am the home page!</h2>
+    <div className='home-page'>
+      <h2 className='home-header'>The One With All the Questions</h2>
+      <img src={frame} alt='gold frame' className='frame'/>
+    </div>
   )
 }
 
