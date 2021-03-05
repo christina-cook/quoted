43db46e539a0d2e461f7e39031c3224f0eb60593
Christina Cook <cmc.cook7@gmail.com>
2021-03-04 20:45:16 -0700

    Add icon to game card

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Card.css';
+import couch from '../../assets/couch.png';
 
 const Card = ({question, answer}) => {
   return (
     <div className='game-card'>
+      <img src={couch} alt='icon' className='card-icon'/>
       <h2 className='question'>{question}</h2>
     </div>
   )
