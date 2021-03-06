f4f46daf4d4cf1bd14b6dfe094f26ac257f29f1c
Christina Cook <cmc.cook7@gmail.com>
2021-03-06 13:51:36 -0700

    Clean up app component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import Header from '../Header/Header';
 import Game from '../Game/Game';
@@ -7,17 +7,8 @@ import HowTo from '../HowTo/HowTo';
 // import Error from '../Error/Error';
 // import Loading from '../Loading/Loading';
 import { Route } from 'react-router-dom';
-import { getQuotes } from '../../ApiCalls';
 
 const App = () => {
-  // const [quotes, setQuotes] = useState([])
-  //
-  // useEffect(() => {
-  //   getQuotes()
-  //     .then(data => setQuotes(data))
-  //     .catch(error => console.log('error', error))
-  // })
-
   return (
     <>
       <Header />
