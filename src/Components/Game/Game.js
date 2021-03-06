05125abbf9cfcb5ca6aeea115cc2dc22c0470c4e
Christina Cook <cmc.cook7@gmail.com>
2021-03-06 13:06:23 -0700

    Add conditional rendering to game

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -4,6 +4,8 @@ import Card from '../Card/Card';
 import arrow from '../../assets/arrow.png';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import Loading from '../Loading/Loading';
+
 
 const Game = ({quotes}) => {
   const [currentNumber, setCurrentNumber] = useState(0)
@@ -72,37 +74,40 @@ const Game = ({quotes}) => {
       </Link>
       <button className='restart' onClick={restartGame}>Start Over</button>
     </div>
-    {!displayScore ?
-    <>
-      <div className='game-container'>
-        <h2 className='question-count'>Question {currentNumber + 1} of {quotes.length}</h2>
-        <div className='game-card-container'>
-          <div className='card-display'>
-            {gameCards[currentNumber]}
+    {!quotes.length && <Loading />}
+    {quotes.length > 0 &&
+      <>
+        <div className='game-container'>
+          <h2 className='question-count'>Question {currentNumber + 1} of {quotes.length}</h2>
+          <div className='game-card-container'>
+            <div className='card-display'>
+              {gameCards[currentNumber]}
+            </div>
+            <img src={arrow} alt='right-arrow' className='arrow' onClick={handleArrowClick}/>
           </div>
-          <img src={arrow} alt='right-arrow' className='arrow' onClick={handleArrowClick}/>
+          <div className='answer-buttons'>
+            <div className='buttons-left'>
+              <button className='character-button' id='Monica' onClick={handleAnswerChoice} disabled={disabled}>Monica</button>
+              <button className='character-button' id='Joey' onClick={handleAnswerChoice} disabled={disabled}>Joey</button>
+              <button className='character-button' id='Phoebe' onClick={handleAnswerChoice} disabled={disabled}>Phoebe</button>
+            </div>
+            <div className='buttons-right'>
+              <button className='character-button' id='Ross' onClick={handleAnswerChoice} disabled={disabled}>Ross</button>
+              <button className='character-button' id='Chandler' onClick={handleAnswerChoice} disabled={disabled}>Chandler</button>
+              <button className='character-button' id='Rachel' onClick={handleAnswerChoice} disabled={disabled}>Rachel</button>
+            </div>
+          </div>
+          <h3 className='answer-message'>{answerMessage}</h3>
         </div>
-        <div className='answer-buttons'>
-          <div className='buttons-left'>
-            <button className='character-button' id='Monica' onClick={handleAnswerChoice} disabled={disabled}>Monica</button>
-            <button className='character-button' id='Joey' onClick={handleAnswerChoice} disabled={disabled}>Joey</button>
-            <button className='character-button' id='Phoebe' onClick={handleAnswerChoice} disabled={disabled}>Phoebe</button>
-          </div>
-          <div className='buttons-right'>
-            <button className='character-button' id='Ross' onClick={handleAnswerChoice} disabled={disabled}>Ross</button>
-            <button className='character-button' id='Chandler' onClick={handleAnswerChoice} disabled={disabled}>Chandler</button>
-            <button className='character-button' id='Rachel' onClick={handleAnswerChoice} disabled={disabled}>Rachel</button>
-          </div>
-        </div>
-        <h3 className='answer-message'>{answerMessage}</h3>
-      </div>
-    </> :
+      </>
+    }
+    {displayScore &&
       <>
         <div className='score-results'>
           <h2 className='score-header'>{`You guessed ${score} out of ${quotes.length} correct!`}</h2>
         </div>
       </>
-      }
+    }
     </>
   )
 }
