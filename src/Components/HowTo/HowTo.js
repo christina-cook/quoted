bfdd58cebcd9f7d9c753a9e3d029cff6e82ddf52
Christina Cook <cmc.cook7@gmail.com>
2021-03-06 14:45:56 -0700

    Add home button to How To Play page

--- src/Components/HowTo/HowTo.js
+++ src/Components/HowTo/HowTo.js
@@ -4,13 +4,20 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className='how-page'>
-      <h2 className='how-header'>How To Play</h2>
-      <p>Instructions go here...</p>
-      <Link to='/game' className='button-link'>
-        <button className='start-button'>Get Started</button>
-      </Link>
-    </div>
+    <>
+      <div className='nav-buttons'>
+          <Link to='/'>
+            <button className='home'>Home</button>
+          </Link>
+        </div>
+      <div className='how-page'>
+        <h2 className='how-header'>How To Play</h2>
+        <p>Instructions go here...</p>
+        <Link to='/game' className='button-link'>
+          <button className='start-button'>Get Started</button>
+        </Link>
+      </div>
+    </>
   )
 }
 
